e81b3c1086812f920454017b8dc3030c65f0d2ef
eyevent <eyevent@USSEAGirls06.ey.net>
2018-07-25 14:01:12 -0700

    first commit

---
+++ NERO.js
@@ -0,0 +1,11 @@
+/*
+  function revealBiography(){
+    console.log("function working");
+    var answer = " NERO is originated in London, United Kingdom. They debute back in 2004, with record labels MTA Records, Interscope Records, and Cherrytree/Interscope Records.";
+    var bio = document.getElenmentById("Yio");
+    bio.innerHTML = answer;
+}
+*/
+    function revealBiography(){
+      document.getElementById("Yio").innerHTML = " NERO is originated in London, United Kingdom. They debute back in 2004, with record labels MTA Records, Interscope Records, and Cherrytree/Interscope Records. ";
+    }
